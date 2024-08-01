b096fa09847ce99cd9c220acb4ab54c2ca4fe050
HARISH23K3 <151422919+HARISH23K3@users.noreply.github.com>
2024-08-01 14:52:10 +0530

    Update admin.py edited comment

--- polls/admin.py
+++ polls/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 
 from polls.models import Choice, Question
 
-# Register your models here.
+# Register your models here. ghgffjhg
 admin.site.register(Question)
 admin.site.register(Choice)
